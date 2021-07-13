530c8f6f0e10b791e112056fd24e097598b15880
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-07-13 19:41:59 +0200

    Updated files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,14 +15,26 @@ import { FormsModule } from '@angular/forms';
 import { UserRegistrationService } from './fetch-api-data.service';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
+    MovieCardComponent,
+    WelcomePageComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
@@ -35,7 +47,8 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
     MatCardModule,
     MatFormFieldModule,
     MatSnackBarModule,
-    [MatCardModule]
+    [MatCardModule],
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
