4b49602c589942507e8ef7b7b650dd31f7b2b4fd
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-07-15 13:46:24 +0200

    Added movie-description dialog

---
+++ src/app/movie-description/movie-description.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-movie-description',
+  templateUrl: './movie-description.component.html',
+  styleUrls: ['./movie-description.component.scss']
+})
+export class MovieDescriptionComponent implements OnInit {
+  /**
+   * Data from the movie-card component
+   * @param data
+   */
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      title: string;
+      ImagePath: string;
+      description: string;
+      director: string;
+      genre: string;
+    }
+
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
