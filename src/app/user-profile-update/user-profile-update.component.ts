a5c2088de11d14c39c3f9e2e06425c4e805e7248
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-07-15 13:44:12 +0200

    Added user profile update logic in user-profile-update

---
+++ src/app/user-profile-update/user-profile-update.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { GetUserService, EditUserService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-user-profile-update',
+  templateUrl: './user-profile-update.component.html',
+  styleUrls: ['./user-profile-update.component.scss']
+})
+export class UserProfileUpdateComponent implements OnInit {
+  @Input() userData = { Username: '', Email: '', Birthdate: '' };
+  // user: any = {};
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: { onSuccess: () => void },
+    public fetchUser: GetUserService,
+    public fetchEditUser: EditUserService,
+    public dialogRef: MatDialogRef<UserProfileUpdateComponent>,
+    public snackBar: MatSnackBar,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * Function sending user profile form input to database to update user account details, then updates user profile in app
+   */
+  editUser(): void {
+    this.fetchEditUser.editUser(this.userData).subscribe((response) => {
+      this.dialogRef.close();
+      console.log(response);
+      localStorage.setItem('user', response.Username);
+      this.snackBar.open('Profile updated!', 'OK', {
+        duration: 2000,
+      });
+      this.data.onSuccess();
+    }, response => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
+        duration: 2000,
+      });
+    });
+    // setTimeout(function () {
+    //   window.location.reload();
+    // }, 1000);
+  }
+
+}
